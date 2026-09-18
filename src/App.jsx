3e099c68e42ff68f00523555a1458cab93d4f2d0
Nayan Mahato <nayanmahato904@gmail.com>
2026-09-18 09:32:56 +0530

    feat: animate cubes with frame-independent rotation
    - Rotate cubes on X and Y axes using useFrame and delta time
    - Add rotation props for an initial tilt
    - Group cubes and shift them upward

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
-import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-function Cube({position , color}){
+function Cube({ rotation, position, color }) {
+  const cubeRef = useRef()
+  
+  useFrame((state , delta)=>{
+    cubeRef.current.rotation.y += 1 * delta //delta matlab last frame aur current frame ke beech kitna time laga. yaha 2 speed hai spped control kar sakte hai
+    cubeRef.current.rotation.x += delta
+  })
   return (
-    <mesh position={position}>
-      <boxGeometry/>
-      <meshStandardMaterial color={color}/>
+    <mesh
+      ref={cubeRef}
+      rotation={rotation} //isse har cube thoda tilted lagega
+      position={position}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={color} />
     </mesh>
-  )
+  );
 }
 const App = () => {
   return (
-    <Canvas camera={{ position : [0 , 0 , 5] }} >
-      <ambientLight intensity={0.5}/>
-      <directionalLight position={[2 , 2, 5]} intensity={2}/>
-      <Cube position={[-2 , 0 , 0]} color={"red"}/>
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 5]} intensity={2} />
+      <group position={[0, 1, 0]}>
         <Cube
-        position={[0, 0, 0]}
-        color="orange"
-      />
+          rotation={[0, Math.PI / 4, 0]}
+          position={[-2, 0, 0]}
+          color={"red"}
+        />
+        <Cube rotation={[0, Math.PI / 4, 0]} position={[0, 0, 0]} color="orange" />
+        <Cube rotation={[0, Math.PI / 4, 0]} position={[2, 0, 0]} color="blue" />
+      </group>
 
-      <Cube
-        position={[2, 0, 0]}
-        color="blue"
-      />
-      <OrbitControls/>
+      <OrbitControls />
     </Canvas>
-  )
-}
+  );
+};
 
-export default App
+export default App;
